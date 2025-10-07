d4c7118e986be6e9d95662633f263c8fe0fbed6d
Md Samadul Hoque <samadul2011@gmail.com>
2025-10-07 21:20:29 +0400

    Update Sales_Vs_Orders.py

--- pages/Sales_Vs_Orders.py
+++ pages/Sales_Vs_Orders.py
@@ -14,10 +14,11 @@ with col3:
 # Page configuration
 st.set_page_config(page_title="Orders vs Sales Difference", layout="wide")
 st.subheader("Developed by :red[Samadul Hoque]")
-with col1_:
-    color="Blue"
-    st.markdown(f"<h1 style='color: {color};'>📊 Orders vs Sales Difference Analysis</h1>", unsafe_allow_html=True)
-    #st.title("📊 Orders vs Sales Difference Analysis")
+with col1:
+    st.title("📊:red [Orders vs Sales Difference Analysis]")
+    #color="Blue"
+    #st.markdown(f"<h1 style='color: {color};'>📊 Orders vs Sales Difference Analysis</h1>", unsafe_allow_html=True)
+    
 
 # Database connection
 @st.cache_resource
@@ -264,6 +265,7 @@ try:
 except Exception as e:
     st.error(f"❌ Error: {str(e)}")
     st.info("Please ensure the database file exists and contains the required tables (Orders and Sales).")
+
 
 
 
