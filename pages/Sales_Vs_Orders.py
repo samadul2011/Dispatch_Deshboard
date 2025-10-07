bba191998f62373bfe99b76be643314648f096b9
Md Samadul Hoque <samadul2011@gmail.com>
2025-10-07 21:27:39 +0400

    Update Sales_Vs_Orders.py

--- pages/Sales_Vs_Orders.py
+++ pages/Sales_Vs_Orders.py
@@ -15,7 +15,7 @@ with col3:
 st.set_page_config(page_title="Orders vs Sales Difference", layout="wide")
 st.subheader("Developed by :red[Samadul Hoque]")
 with col1:
-    st.subheader("📊 :red[Orders vs Sales]")
+    st.subheader(":red[Orders vs Sales]  📊 ")
 with col2:
     st.subheader(" :green[Difference Analysis]")
     #color="Blue"
@@ -267,6 +267,7 @@ try:
 except Exception as e:
     st.error(f"❌ Error: {str(e)}")
     st.info("Please ensure the database file exists and contains the required tables (Orders and Sales).")
+
 
 
 
