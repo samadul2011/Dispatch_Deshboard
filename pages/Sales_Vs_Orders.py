b4b4022810467d0b32191f2ede638b14539e6f43
Md Samadul Hoque <samadul2011@gmail.com>
2025-10-07 21:07:24 +0400

    Update Sales_Vs_Orders.py

--- pages/Sales_Vs_Orders.py
+++ pages/Sales_Vs_Orders.py
@@ -7,7 +7,11 @@ import os
 
 if st.sidebar.button("🔄 Refresh Data"):
     st.cache_data.clear()  # clears cache
-
+col1, col2, col3 = st.columns([1, 2, 1])
+with col3:
+    st.image("https://raw.githubusercontent.com/samadul2011/Dispatch_Deshboard/main/AtyabLogo.png", width=200)
+with col2:
+    st.subheader("Developed by :red[Samadul Hoque]")
 # Page configuration
 st.set_page_config(page_title="Orders vs Sales Difference", layout="wide")
 color="Blue"
@@ -259,4 +263,5 @@ try:
 except Exception as e:
     st.error(f"❌ Error: {str(e)}")
     st.info("Please ensure the database file exists and contains the required tables (Orders and Sales).")
+
 
